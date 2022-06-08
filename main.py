a507a136096ed0ec16bd2a24ad6ac2e787ff0909
m1cro4el <98770773+m1cro4el@users.noreply.github.com>
2022-06-08 11:14:31 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,15 @@
+<<<<<<< HEAD
+print ('hello') 
+=======
+from NYPizzaStore import NYPizzaStore
+from NYStyleCheesePizza import NYStyleCheesePizza
+
+
+nyStore = NYPizzaStore()
+
+pizza = nyStore.orderPizza('cheese')
+
+print('nastya zakazala', pizza.getName())
+
+               
+>>>>>>> 210702c (7)
